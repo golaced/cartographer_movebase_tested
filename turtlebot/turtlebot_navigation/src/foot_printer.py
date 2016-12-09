acd7d76dc820d0d3dc65313e045bf8b2152f9781
zhouwei <zhouwei.a@outlook.com>
2016-12-09 15:48:23 +0800

    fix the new foot_printer.py bugs

--- turtlebot/turtlebot_navigation/src/foot_printer.py
+++ turtlebot/turtlebot_navigation/src/foot_printer.py
@@ -10,9 +10,8 @@ from nav_msgs.msg import *
 
 if __name__ == '__main__':
 
-		int xAnt=0.0
-        int yAnt=0.0
-        int cont=0
+	xAnt=0.0
+        yAnt=0.0
 
         rospy.init_node('foot_printer')
 
@@ -38,26 +37,13 @@ if __name__ == '__main__':
 	        pose.pose.position.x = trans[0]
 	        pose.pose.position.y = trans[1]
 
-	        '''
-	        #pose.pose.orientation.x = float(data.pose.pose.orientation.x)
-	        #pose.pose.orientation.y = float(data.pose.pose.orientation.y)
-	        #pose.pose.orientation.z = float(data.pose.pose.orientation.z)
-	        #pose.pose.orientation.w = float(data.pose.pose.orientation.w)
-	        '''
-
-	        if (xAnt != pose.pose.position.x and yAnt != pose.pose.position.y):
+	        if (xAnt != pose.pose.position.x or yAnt != pose.pose.position.y):
 	                pose.header.seq = path.header.seq + 1
-	                path.header.frame_id="odom"
+	                path.header.frame_id="/map"
 	                path.header.stamp=rospy.Time.now()
 	                pose.header.stamp = path.header.stamp
 	                path.poses.append(pose)
-
-	        #cont=cont+1
-	        #rospy.loginfo("Valor del contador: %i" % cont)
-	        #if cont>max_append:
-		    #	path.poses.pop(0)
-
-	        pub.publish(path)
-	        xAnt=pose.pose.orientation.x
+	                pub.publish(path)
+	        xAnt=pose.pose.position.x
 	        yAnt=pose.pose.position.y
        		rate.sleep()
